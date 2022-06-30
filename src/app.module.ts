3b9a381f5b9a88d59179dc6748137a21406170b7
JustinAMB <Justinmb@outlook.es>
2022-06-30 12:58:55 -0600

    se importa el modulo de bbddd

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PersonsController } from './persons/persons.controller';
 import { PersonService } from './services/person/person.service';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [],
+  imports: [DatabaseModule],
   controllers: [AppController, PersonsController],
   providers: [AppService, PersonService],
 })
