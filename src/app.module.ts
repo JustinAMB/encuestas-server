0fef07217f586eede944d303344257131234c43d
JustinAMB <Justinmb@outlook.es>
2022-07-01 02:04:54 -0600

    se configura las entidades

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import {ConfigModule} from '@nestjs/config';
 import {TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
 
 
 @Module({
@@ -14,9 +15,10 @@ import { AppService } from './app.service';
       username: process.env.DB_USER,
       password: process.env.DB_PASS,
       database: process.env.DB_NAME,
-      entities:[],
+      entities:['dist/**/*.entity.js'],
       synchronize:true
-    }),],
+    }),
+    UsersModule,],
   controllers: [AppController,],
   providers: [AppService],
 })
